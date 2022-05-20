252fc95457a5236527633bd5ed1bb8860960eeaa
Adam Wilcock <adamwilcock10@gmail.com>
2022-05-20 10:31:07 +0100

    Added validation classes and moved create client event to business logic layer

--- src/InvoiceGenerator/InvoiceGenerator.BusinessLogic/InvoiceDBContext.cs
+++ src/InvoiceGenerator/InvoiceGenerator.BusinessLogic/InvoiceDBContext.cs
@@ -5,16 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InvoiceGenerator
+namespace InvoiceGenerator.BusinessLogic
 {
     public class InvoiceDBContext : DbContext
     {
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=tcp:invoice-generator.database.windows.net,1433;Initial Catalog=invoice-generator;Persist Security Info=False;User ID=ef-user;Password=x3Dh0zfg2s1k;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
 
-        public DbSet<Models.Client> Clients { get; set; }
+        public DbSet<Client> Clients { get; set; }
     }
 }
