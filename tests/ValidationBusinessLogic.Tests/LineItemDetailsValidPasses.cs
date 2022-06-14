0116f8ace0b96e2add932ea9fb259911ba05c11f
Adam-Wilcock <adamwilcock10@gmail.com>
2022-06-14 10:14:15 +0100

    Line Item Happy Path Unit Test Complete

---
+++ tests/ValidationBusinessLogic.Tests/LineItemDetailsValidPasses.cs
@@ -0,0 +1,39 @@
+﻿using InvoiceGenerator.BusinessLogic;
+using InvoiceGenerator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ValidationBusinessLogic.Tests
+{
+    public class LineItemDetailsValidPasses
+    {
+        [Fact]
+        public void Check_If_NoError_Is_Returned_When_Valid_Details_Input()
+        {
+            // Arrange
+            var expectedResult = new LineItemViewModelValidationResult()
+            {
+                LineItemDescriptionValidationMessage = "No error!",
+                LineItemCostValidationMessage = "No error!",
+                LineItemQuantityValidationMessage = "No error!"
+            };
+            var viewModel = new LineItemViewModel()
+            {
+                Description = "Butter",
+                CostPer = "1.65",
+                Quantity = "3"
+            };
+            var SUT = new ValidationBLogic();
+            // Act
+            var result = SUT.validateLineItemDetails(viewModel);
+            // Assert
+            Assert.Equal(expectedResult.LineItemDescriptionValidationMessage, result.LineItemDescriptionValidationMessage);
+            Assert.Equal(expectedResult.LineItemCostValidationMessage, result.LineItemCostValidationMessage);
+            Assert.Equal(expectedResult.LineItemQuantityValidationMessage, result.LineItemQuantityValidationMessage);
+        }
+    }
+}
