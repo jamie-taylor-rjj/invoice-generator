93dc6439fe2b8f1b8b058b365fc6d828b9c93746
Adam-Wilcock <adamwilcock10@gmail.com>
2022-06-14 11:48:29 +0100

    Completed VATInvalidPasses Unit Tests

---
+++ tests/ValidationBusinessLogic.Tests/VATInvalidPasses.cs
@@ -0,0 +1,54 @@
+﻿using InvoiceGenerator.BusinessLogic;
+using InvoiceGenerator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ValidationBusinessLogic.Tests
+{
+    public class VATInvalidPasses
+    {
+        [Fact]
+        public void Check_If_Error_Is_Returned_When_Invalid_VAT_Input()
+        {
+            // Arrange
+            var expectedResult = new InvoiceViewModelValidationResult()
+            {
+                VATValidationMessage = "VAT/Sales Tax must be an integer!"
+            };
+            var viewModel = new InvoiceViewModel()
+            {
+                VatRate = "Test"
+            };
+            var SUT = new ValidationBLogic();
+            // Act
+            var result = SUT.checkVAT(viewModel);
+            // Assert
+            Assert.Equal(expectedResult.VATValidationMessage, result.VATValidationMessage);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Check_VAT_Null_Or_Empty(string VAT)
+        {
+            // Arrange
+            var expectedResult = new InvoiceViewModelValidationResult()
+            {
+                VATValidationMessage = "VAT/Sales Tax must be an integer!"
+            };
+            var input = new InvoiceViewModel()
+            {
+                VatRate = VAT
+            };
+            var SUT = new ValidationBLogic();
+            // Act
+            var result = SUT.checkVAT(input);
+            // Assert
+            Assert.Equal(expectedResult.VATValidationMessage, result.VATValidationMessage);
+        }
+    }
+}
